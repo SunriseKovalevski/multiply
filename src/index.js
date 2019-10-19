62035ce8f485458e2290afca7f42329b6e88e5dc
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-10-19 22:47:21 +0300

    resolve from codewars

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  if (!first || !second || typeof(first) != "number" || typeof(second) != "number") {
+    return 0;
+  }
+  return first * second;
 }
