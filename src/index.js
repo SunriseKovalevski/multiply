afddc44c73bbcefa773c71f748dcf82d572b89b4
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-10-19 22:59:24 +0300

    first work commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function multiply(first, second) {
-  if (!first || !second || typeof(first) != "number" || typeof(second) != "number") {
-    return 0;
-  }
-  return first * second;
-}
+  return first * second + "";
+};
